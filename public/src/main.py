da67e9e82f6ff78ad368b22d091d50f68d2af1c6
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-01-27 21:54:31 +0100

    Moved everything into place, modified gitingore to not the pychache

--- public/src/main.py
+++
@@ -1,8 +0,0 @@
-from textnode import *
-
-def main():
-    textnode = TextNode("this is a test string", TextType.NORMAL, "https://www.boot.dev")
-    print(textnode)
-
-
-main()
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    textnode = TextNode("this is a test string", TextType.TEXT, "https://www.boot.dev")
+    print(textnode)
+
+
+main()

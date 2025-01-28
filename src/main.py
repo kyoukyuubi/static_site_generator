05e1272825c3924990aff2863282e5473309eb4a
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-01-28 12:12:42 +0100

    Added test.sh and unittests

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from textnode import *
 
 def main():
     textnode = TextNode("this is a test string", TextType.TEXT, "https://www.boot.dev")
+    othernode = TextNode("Another text node", TextType.TEXT)
+    print(textnode == othernode)
     print(textnode)
 
 

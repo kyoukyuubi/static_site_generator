12d354c545070ff26f22d7beab5f401ba6865809
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-01-29 17:18:10 +0100

    Adde text to text_nodes plus a unittest to make sure it works

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from htmlnode import *
 from inline_markdown import *
 
 def main():
-    pass
+    text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    print(text_to_textnodes(text))
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -20,7 +20,7 @@ class TextNode():
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
     
     def __repr__(self):
-        return f"TextNode({self.text},{self.text_type.value},{self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
 def text_node_to_html_node(text_node):
     #Use match case, to see what text_type the inputted node is and outputs the current node with the currect tag and values, if no matches raise an ValueError

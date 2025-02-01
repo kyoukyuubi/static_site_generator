1b8a0aeaf4a5525f6ab57461d00c0bae806ba46e
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-02-02 00:44:38 +0100

    Modified the main.py to be able to delete everything in public (or create it if it doesn't exist) and copy everything from static over to it

--- src/main.py
+++ src/main.py
@@ -1,31 +1,35 @@
-from textnode import *
-from htmlnode import *
-from inline_markdown import *
-from block_markdown import *
-from functions import *
+import os
+import shutil
+
+def copy_recursive(source, destination):
+    #Clean the destination if it already exists
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+
+    #Create the destination directory fresh
+    os.mkdir(destination)
+
+    #Store the items in the source dir
+    dir_list = os.listdir(source)
+
+    for item in dir_list:
+        #Store the full patch of the item
+        full_path = os.path.join(source, item)
+
+        #Check if full_path findes a file
+        if os.path.isfile(full_path):
+            shutil.copy(full_path, destination)
+        #Check if full_path findes a subdirectory
+        elif os.path.isdir(full_path):
+            #Call itself on the subdirectory
+            copy_recursive(full_path, os.path.join(destination, item))
+    return True
 
 def main():
-    markdown = """# Header one
-
-This is a paragraph with some *italic* and **bold** text.
-
-* List item 1
-* List item 2
-* List item 3
-
-1. Ordered item 1
-2. Ordered item 2
-3. Ordered item 3
-
-> This is a block quote
-> with multiple lines
-
-```python
-def hello_world():
-    print("Hello World!")
-```
-"""
-    node = markdown_to_html_node(markdown)
-    print(node)
+    try:
+        if copy_recursive("static", "public"):
+            print("Operation succeded!")
+    except Exception as e:
+        print (f"Execption Caught: {e}")
 
 main()
